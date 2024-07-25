482b1f80ef9b2efc08eaed32110d0d01b353178b
justin045 <justinlu045@gmail.com>
2024-07-25 11:21:05 +0800

    chore: implementation location library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,7 @@ dependencies {
     implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.35.0-alpha")
 }
