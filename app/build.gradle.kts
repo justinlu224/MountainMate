b9c1ee61096e4fa724974469eb5da79f83e2ce5d
justin045 <justinlu045@gmail.com>
2024-08-11 20:20:07 +0800

    chore: set up fastlane for app distribution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.google.dagger.hilt.android")
     kotlin("plugin.serialization") version "2.0.0"
     id("com.google.gms.google-services")
+    id("com.google.firebase.appdistribution")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,6 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
     // Add the dependency for the Google services Gradle plugin
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.appdistribution") version "5.0.0" apply false
 
 }
